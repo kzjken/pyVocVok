e648a740df79bcb7aa3060cd960c7216c5c3c312
ZKang <kzjken@gmail.com>
2022-10-03 16:28:04 +0200

    reconstruct code

--- learnEnDe.py
+++ learnEnDe.py
@@ -1,18 +1,26 @@
-import random
+import os
 
-# read list
-with open('vocList.txt', 'r', encoding='UTF-8') as fileEn4:
-    fileEn4Lines = fileEn4.readlines()
+def readTxt(txtFile):
+    """Function reading text file."""
+    with open(txtFile, 'r', encoding='UTF-8') as fileIn:
+        lstFileLines = fileIn.readlines()
+    return lstFileLines
 
-# # search
-# for i in range(50):
-#     # get random content
-#     searchContent = random.choice(fileEn4Lines)[0:-1]
-#     print(str(i) + ": " + searchContent)
+def saveFile(csvFile, lstIn):
+    """Function saving csv file."""
+    with open(csvFile, 'w', encoding='UTF-8') as fileOut:
+        for content in lstIn:
+            fileOut.write(content)
 
-for idx, vocabulary in enumerate (fileEn4Lines):
-    # if idx <= 10:
-    #     print(str(idx) + ': ' + vocabulary[0:-1] + '  space No.: ' + str(vocabulary[0:-1].count(' ')))
-    if vocabulary[0:-1].count(' ') > 1:     
-        print(str(idx) + ': ' + vocabulary[0:-1] + '  space No.: ' + str(vocabulary[0:-1].count(' ')))
-        
+def main():
+    os.system('cls')
+    lstEnSrc = readTxt('vocList.txt')
+    for idx, vocabulary in enumerate (lstEnSrc):
+        # if vocabulary[0:-1].count(' ') > 1:
+        #     print(str(idx) + ': ' + vocabulary[0:-1] + '  space No.: ' + str(vocabulary[0:-1].count(' ')))
+        if len(vocabulary[0:-1]) == 1:           
+            print(str(idx) + ': ' + vocabulary[0:-1])
+    # saveFile('vocListDest.csv', lstEnSrc)
+
+if __name__ == "__main__":
+    main()
